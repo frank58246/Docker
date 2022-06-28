cf65291504ec07a42d44adc5a10fb657313e58d7
Frank <Frank@DESKTOP-FA8EJCS>
2022-06-28 23:28:08 +0800

    add k6

--- k6/scripts/rabbit.js
+++ k6/scripts/rabbit.js
@@ -6,10 +6,10 @@ export let options = {
       // Ramp-up from 1 to 5 virtual users (VUs) in 5s
       // { duration: "60", target: 2000 },
 
-      { duration: "10s", target: 10 },
+      { duration: "60s", target: 10 },
 
       // Stay at rest on 5 VUs for 10s
-      { duration: "60s", target: 1000 },
+      // { duration: "60s", target: 100 },
 
       // // Ramp-down from 5 to 0 VUs for 5s
       // { duration: "5s", target: 10 }
@@ -17,10 +17,11 @@ export let options = {
 };
 
 export default function () {
-  var url = "http://host.docker.internal:30005/Message/SendUpdateHouse"
-  let data = {"caption":"abc","upPrice":0,"downPrice":0,"updateTime":"2022-06-25T15:09:02.955Z"};
+  var url = "http://host.docker.internal:40002/Message/SendUpdateHouse"
+  let data = {"caption":"vvvv","upPrice":0,"downPrice":0,"updateTime":"2022-06-25T15:09:02.955Z"};
 
-  let response = http.get(url, JSON.stringify(data), {
+  // url = "http://host.docker.internal:30005/Message/echo?value=111"
+  let response = http.post(url, JSON.stringify(data), {
     headers: { 'Content-Type': 'application/json' },
   });  
   
